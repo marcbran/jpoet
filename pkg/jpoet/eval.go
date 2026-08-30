8f7fbb3707d96932d7ff28c29d7a51c248ed0646
Marc Brandenburg <git@marcbrandenburg.net>
2026-08-30 11:17:24 +0200

    feat: reusable env for multiple evals

--- pkg/jpoet/eval.go
+++ pkg/jpoet/eval.go
@@ -1,82 +1,85 @@
 package jpoet
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"io/fs"
-	"os"
-	"path/filepath"
 
 	"github.com/google/go-jsonnet"
 	"github.com/google/go-jsonnet/ast"
 )
 
-type Option func(*evalConfig)
-
-type evalConfig struct {
-	vmOpts  []func(*jsonnet.VM)
-	closers []io.Closer
-
-	importer CompoundImporter
-	contents map[string]jsonnet.Contents
-
-	nodeInput    *ast.Node
-	snippetInput *snippetInput
-	fileInput    *string
-
-	writerOutput    io.Writer
-	valueOutput     any
-	directoryOutput string
-
-	serializedFormat bool
-
-	errs []error
-}
-
-type snippetInput struct {
-	filename string
-	snippet  string
-}
-
-func newEvalConfig() *evalConfig {
-	return &evalConfig{
-		contents:         make(map[string]jsonnet.Contents),
-		writerOutput:     os.Stdout,
-		serializedFormat: true,
-	}
-}
-
 func Eval(opts ...Option) error {
-	c := newEvalConfig()
+	c := newConfig()
 	for _, opt := range opts {
 		opt(c)
 	}
 	return c.eval()
 }
 
+type Option func(*config)
+
+type config struct {
+	envConfig
+	evalConfig
+
+	errs []error
+}
+
+func newConfig() *config {
+	return &config{
+		envConfig:  newEnvConfig(),
+		evalConfig: newEvalConfig(),
+	}
+}
+
+func (c *config) eval() error {
+	defer func() {
+		for _, closer := range c.closers {
+			err := closer.Close()
+			if err != nil {
+				c.errs = append(c.errs, err)
+			}
+		}
+	}()
+	vm := c.buildVM()
+	err := c.run(vm)
+	if err != nil {
+		c.errs = append(c.errs, err)
+	}
+	return c.error()
+}
+
+func (c *config) error() error {
+	if len(c.errs) == 0 {
+		return nil
+	}
+	if len(c.errs) == 1 {
+		return fmt.Errorf("failed to evaluate Jsonnet: %w", c.errs[0])
+	}
+	return fmt.Errorf("failed to evaluate Jsonnet: %s", c.errs)
+}
+
 func TLAVar(key, val string) Option {
-	return func(c *evalConfig) {
+	return func(c *config) {
 		c.vmOpts = append(c.vmOpts, func(vm *jsonnet.VM) { vm.TLAVar(key, val) })
 	}
 }
 
 func TLACode(key, val string) Option {
-	return func(c *evalConfig) {
+	return func(c *config) {
 		c.vmOpts = append(c.vmOpts, func(vm *jsonnet.VM) { vm.TLACode(key, val) })
 	}
 }
 
 func TLANode(key string, node ast.Node) Option {
-	return func(c *evalConfig) {
+	return func(c *config) {
 		c.vmOpts = append(c.vmOpts, func(vm *jsonnet.VM) { vm.TLANode(key, node) })
 	}
 }
 
 func NodeInput(node ast.Node) Option {
-	return func(c *evalConfig) {
+	return func(c *config) {
 		c.nodeInput = &node
 		c.snippetInput = nil
 		c.fileInput = nil
@@ -84,7 +87,7 @@ func NodeInput(node ast.Node) Option {
 }
 
 func SnippetInput(filename, snippet string) Option {
-	return func(c *evalConfig) {
+	return func(c *config) {
 		c.nodeInput = nil
 		c.snippetInput = &snippetInput{filename, snippet}
 		c.fileInput = nil
@@ -92,7 +95,7 @@ func SnippetInput(filename, snippet string) Option {
 }
 
 func FileInput(filename string) Option {
-	return func(c *evalConfig) {
+	return func(c *config) {
 		c.nodeInput = nil
 		c.snippetInput = nil
 		c.fileInput = &filename
@@ -100,7 +103,7 @@ func FileInput(filename string) Option {
 }
 
 func Importer(i jsonnet.Importer) Option {
-	return func(c *evalConfig) {
+	return func(c *config) {
 		c.importer.Importers = append(c.importer.Importers, i)
 	}
 }
@@ -114,13 +117,13 @@ func FSImport(f fs.FS) Option {
 }
 
 func StringImport(filename, value string) Option {
-	return func(c *evalConfig) {
+	return func(c *config) {
 		c.contents[filename] = jsonnet.MakeContents(value)
 	}
 }
 
 func WithNativeFunction(f *jsonnet.NativeFunction) Option {
-	return func(c *evalConfig) {
+	return func(c *config) {
 		if f == nil {
 			return
 		}
@@ -129,14 +132,14 @@ func WithNativeFunction(f *jsonnet.NativeFunction) Option {
 }
 
 func WithPlugin(p *Plugin) Option {
-	return func(c *evalConfig) {
+	return func(c *config) {
 		c.closers = append(c.closers, p)
 		WithNativeFunction(p.NativeFunction())(c)
 	}
 }
 
 func WithPluginSet(plugins ...*Plugin) Option {
-	return func(c *evalConfig) {
+	return func(c *config) {
 		for _, p := range plugins {
 			WithPlugin(p)(c)
 		}
@@ -144,7 +147,7 @@ func WithPluginSet(plugins ...*Plugin) Option {
 }
 
 func WriterOutput(w io.Writer) Option {
-	return func(c *evalConfig) {
+	return func(c *config) {
 		c.writerOutput = w
 		c.valueOutput = nil
 		c.directoryOutput = ""
@@ -152,7 +155,7 @@ func WriterOutput(w io.Writer) Option {
 }
 
 func ValueOutput(out any) Option {
-	return func(c *evalConfig) {
+	return func(c *config) {
 		c.writerOutput = nil
 		c.valueOutput = out
 		c.directoryOutput = ""
@@ -160,7 +163,7 @@ func ValueOutput(out any) Option {
 }
 
 func DirectoryOutput(dir string) Option {
-	return func(c *evalConfig) {
+	return func(c *config) {
 		c.writerOutput = nil
 		c.valueOutput = nil
 		c.directoryOutput = dir
@@ -168,170 +171,7 @@ func DirectoryOutput(dir string) Option {
 }
 
 func Serialize(s bool) Option {
-	return func(c *evalConfig) {
+	return func(c *config) {
 		c.serializedFormat = s
 	}
-}
-
-func (c *evalConfig) hasInput() bool {
-	return c.nodeInput != nil || c.snippetInput != nil || c.fileInput != nil
-}
-
-func (c *evalConfig) error() error {
-	if len(c.errs) == 0 {
-		return nil
-	}
-	if len(c.errs) == 1 {
-		return fmt.Errorf("failed to evaluate Jsonnet: %w", c.errs[0])
-	}
-	return fmt.Errorf("failed to evaluate Jsonnet: %s", c.errs)
-}
-
-func (c *evalConfig) eval() error {
-	defer func() {
-		for _, closer := range c.closers {
-			err := closer.Close()
-			if err != nil {
-				c.errs = append(c.errs, err)
-			}
-		}
-	}()
-	if !c.hasInput() {
-		c.errs = append(c.errs, errors.New("missing input"))
-		return c.error()
-	}
-	if len(c.contents) > 0 {
-		c.importer.Importers = append(c.importer.Importers, &MemoryImporter{
-			Data: c.contents,
-		})
-	}
-	vm := jsonnet.MakeVM()
-	for _, opt := range c.vmOpts {
-		opt(vm)
-	}
-	if len(c.importer.Importers) > 0 {
-		vm.Importer(c.importer)
-	}
-
-	var serializedJson string
-	var err error
-	if c.nodeInput != nil {
-		serializedJson, err = vm.Evaluate(*c.nodeInput)
-		if err != nil {
-			c.errs = append(c.errs, err)
-			return c.error()
-		}
-	} else if c.snippetInput != nil {
-		serializedJson, err = vm.EvaluateAnonymousSnippet(c.snippetInput.filename, c.snippetInput.snippet)
-		if err != nil {
-			c.errs = append(c.errs, err)
-			return c.error()
-		}
-	} else if c.fileInput != nil {
-		serializedJson, err = vm.EvaluateFile(*c.fileInput)
-		if err != nil {
-			c.errs = append(c.errs, err)
-			return c.error()
-		}
-	}
-
-	if c.writerOutput != nil {
-		output := serializedJson
-		if !c.serializedFormat {
-			err := json.Unmarshal([]byte(serializedJson), &output)
-			if err != nil {
-				c.errs = append(c.errs, err)
-				return c.error()
-			}
-		}
-		_, err := c.writerOutput.Write([]byte(output))
-		if err != nil {
-			c.errs = append(c.errs, err)
-			return c.error()
-		}
-	} else if c.valueOutput != nil {
-		if c.serializedFormat {
-			c.valueOutput = serializedJson
-		} else {
-			err := json.Unmarshal([]byte(serializedJson), c.valueOutput)
-			if err != nil {
-				c.errs = append(c.errs, err)
-				return c.error()
-			}
-		}
-	} else if c.directoryOutput != "" {
-		var entries map[string]any
-		err = json.Unmarshal([]byte(serializedJson), &entries)
-		if err != nil {
-			c.errs = append(c.errs, err)
-			return c.error()
-		}
-		err = writeEntries(c.directoryOutput, entries, c.serializedFormat)
-		if err != nil {
-			c.errs = append(c.errs, err)
-			return c.error()
-		}
-	}
-	return c.error()
-}
-
-func writeEntries(directory string, entries map[string]any, serialized bool) error {
-	for filename, c := range entries {
-		switch content := c.(type) {
-		case map[string]any:
-			err := writeEntries(filepath.Join(directory, filename), content, serialized)
-			if err != nil {
-				return err
-			}
-		default:
-			err := writeFile(filepath.Join(directory, filename), content, serialized)
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func writeFile(filename string, content any, serialized bool) error {
-	var fileContent []byte
-	if serialized {
-		var err error
-		fileContent, err = json.Marshal(content)
-		if err != nil {
-			return err
-		}
-	} else {
-		stringContent, ok := content.(string)
-		if !ok {
-			return fmt.Errorf("expect string when writing output to file: %s, but got %T", filename, content)
-		}
-		fileContent = []byte(stringContent)
-	}
-
-	_, err := os.Stat(filename)
-
-	if err != nil && !errors.Is(err, fs.ErrNotExist) {
-		return err
-	}
-
-	if err == nil {
-		existingContent, err := os.ReadFile(filename)
-		if err != nil {
-			return err
-		}
-		if bytes.Equal(existingContent, fileContent) {
-			return nil
-		}
-	}
-
-	err = os.MkdirAll(filepath.Dir(filename), 0755)
-	if err != nil {
-		return err
-	}
-	err = os.WriteFile(filename, fileContent, 0666)
-	if err != nil {
-		return err
-	}
-	return nil
 }
